d639bc9cc511df7caaeeb9e8786484bae65e9fde
phaptk95 <trankhanhphap95@gmail.com>
2019-08-06 14:27:41 +0700

    Đây là file java test Hibernate

--- JavaOOP/src/main/java/QLCuaHang/LoaiHang.java
+++ JavaOOP/src/main/java/QLCuaHang/LoaiHang.java
@@ -1,5 +1,57 @@
 package QLCuaHang;
+import QLCuaHang.NhomHang;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
+@Entity
+@Table(name="tbl_loaihang",  uniqueConstraints = {@UniqueConstraint(columnNames = {"LH_ID"})})
 public class LoaiHang {
 
-}
+	
+	private Integer LH_ID;
+	private String LH_Ten;
+	private Set<HangHoa> HangHoa = new HashSet<HangHoa>(0);
+	
+
+		public LoaiHang() {
+			
+		}
+		public LoaiHang(Integer IDNH,Integer ID,String TenLH) {
+				NhomHang. = IDNH;
+				this.LH_ID = ID;
+				this.LH_Ten = TenLH;
+			
+		}
+		@Id
+		@OneToMany
+		@Column(name="LH_ID")
+		public Integer GetLH_ID(){
+			return LH_ID;
+		}
+		public void SetLH_ID(Integer LH_ID) {
+			this.LH_ID = LH_ID;
+		} // ID Loại Hàng
+		
+		public String GetTenLH() {
+			return LH_Ten;
+		}
+		public void SetTenLH(String LH_ten) {
+			this.LH_Ten = LH_ten;
+		} // Tên Loại Hàng
+		
+		@Id
+		@Column(name="ID_Nhom")
+		public Integer Get
+
+	}
+	
+
--- JavaOOP/src/main/java/QLCuaHang/NhomHang.java
+++ JavaOOP/src/main/java/QLCuaHang/NhomHang.java
@@ -1,11 +1,45 @@
 package QLCuaHang;
 import java.util.*;
-import java.lang.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
 
 @Entity
-@Table(name="tbl_nhomhang",  uniqueConstraints = {@UniqueConstraints(columnNames = {"NH_ID"})})
+@Table(name="tbl_nhomhang",  uniqueConstraints = {@UniqueConstraint(columnNames = {"NH_ID"})})
 public class NhomHang {
 private Integer NHID;
 private String NH_Ten;
+private Set<LoaiHang> LHang = new HashSet<LoaiHang>(0);
+
+	public NhomHang() {
+		
+	}
+	public NhomHang(Integer ID,String TenNH) {
+	
+			this.NHID = ID;
+			this.NH_Ten = TenNH;
+		
+	}
+	@Id
+	@OneToMany
+	@Column(name="ID_NH")
+	public Integer GetNHID(){
+		return NHID;
+	}
+	public void SetIDNH(Integer NHID) {
+		this.NHID = NHID;
+	}
+	@Column(name="Ten_NH")
+		public String GetTenNH() {
+		return NH_Ten;
+	}
+		public void SetTenNH(String TenNH) {
+			this.NH_Ten = TenNH;		}
+	
 
 }
